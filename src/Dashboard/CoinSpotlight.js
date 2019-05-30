d62983fd92731dbfa7b910d782cff2df416835a9
romene <rogerio.eire@gmail.com>
2019-05-31 10:27:38 +1200

    add spotlight coin

---
+++ src/Dashboard/CoinSpotlight.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components' 
+import {Tile} from '../Shared/Tile'
+import { AppContext } from '../AppProvider';
+import CoinImage from '../Shared/CoinImage'
+
+const CoinSpotlight = () => {
+    return (
+        <AppContext>
+        {({currentFavorite, coinList}) => 
+        <Tile>
+           <SpotlightName>{coinList[currentFavorite].CoinName} </SpotlightName>
+           <CoinImage spotlight coin={coinList[currentFavorite]} />
+        </Tile>
+        }
+        </AppContext>
+    )
+}
+
+export default CoinSpotlight
+
+
+const SpotlightName = styled.h2`
+text-align: center;
+`
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 import Page from '../Shared/Page'
 import PriceGrid from './PriceGrid'
+import CoinSpotlight from './CoinSpotlight';
 
 export default function() {
   return <Page name="dashboard">
     <PriceGrid />
+    <ChartGrid>
+    <CoinSpotlight />
+    <div>Chart goes here!</div>
+    </ChartGrid>
   </Page>
 }
+
+
+// Styled components 
+
+const ChartGrid = styled.div`
+display: grid;
+margin-top: 20px;
+grid-gap: 15px;
+grid-template-columns: 1fr 3fr;
+
+`
